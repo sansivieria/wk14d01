afd9c444ebedebb98a5fb24f72783255ca0a91fd
Forest Traylor <foresttraylor@gmail.com>
2021-07-20 16:01:01 -0700

    verbal questions completed

---
+++ solutions.js
@@ -0,0 +1,5 @@
+// parameter is a variable thats defined in a method,
+// argument is the data passed as the method's parameter
+
+// console.log only displays output on console
+// while return ends a function and returns the value
